2887cfc93ffd62ae5a0c72c0bef1f98036511b3e
Shih-Wen Su <sushi@summeridea.com>
2015-10-03 04:36:20 +0800

    add accident data, ingredient marker color

--- public/js/main-map.js
+++ public/js/main-map.js
@@ -3,8 +3,8 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiZmx5YWtpdGUiLCJhIjoiY2lmOW9ycmI1MWI5anM5bHgze
 // load in non-clustered markers from the layer. Instead we add just the
 // backing tileLayer, and then use the featureLayer only for its data.
 var map = L.mapbox.map('map')
-    .setView([25.034, 121.54], 13)
-    .addLayer(L.mapbox.tileLayer('mapbox.streets'));
+  .setView([25.034, 121.54], 13)
+  .addLayer(L.mapbox.tileLayer('mapbox.streets'));
 
 var overlays = L.layerGroup().addTo(map);
 
@@ -16,11 +16,11 @@ var layers;
 // Since featureLayer is an asynchronous method, we use the `.on('ready'`
 // call to only use its marker data once we know it is actually loaded.
 L.mapbox.featureLayer()
-    .loadURL('/data/crime-hot-spot.json')
-    .on('ready', function(e) {
-        layers = e.target;
-        showSpots();
-    });
+  .loadURL('/data/hot-spot.json')
+  .on('ready', function(e) {
+    layers = e.target;
+    showSpots();
+  });
 
 var filters = document.getElementById('colors').filters;
 
@@ -30,20 +30,39 @@ var filters = document.getElementById('colors').filters;
 // Thus we distill filtering down to its essential part: an 'if' statement
 // in a loop.
 function showSpots() {
-    // first collect all of the checked boxes and create an array of strings
-    // like ['green', 'blue']
-    var list = [];
-    for (var i = 0; i < filters.length; i++) {
-        if (filters[i].checked) list.push(filters[i].value);
+  // first collect all of the checked boxes and create an array of strings
+  // like ['green', 'blue']
+  var list = [];
+  for (var i = 0; i < filters.length; i++) {
+    if (filters[i].checked) list.push(filters[i].value);
+  }
+  // then remove any previously-displayed marker groups
+  overlays.clearLayers();
+  // create a new marker group
+  var clusterGroup = new L.MarkerClusterGroup({
+    iconCreateFunction: function(cluster) {
+      var c = cluster.getChildCount();
+      var icon = c >= 100? 'danger': c;
+      var color, s;
+      // ingredient marker color
+      if(c>=100){
+        color = '#ff0000';
+      }else{
+        s = Math.floor((100-c)*1.2).toString(16)
+        s = s.length == 1? '0'+s:s;
+        color = '#f2' + s +'11';
+      }
+      return L.mapbox.marker.icon({
+        // show the number of markers in the cluster on the icon.
+        'marker-symbol': icon,
+        'marker-color': color
+      });
     }
-    // then remove any previously-displayed marker groups
-    overlays.clearLayers();
-    // create a new marker group
-    var clusterGroup = new L.MarkerClusterGroup().addTo(overlays);
-    // and add any markers that fit the filtered criteria to that group.
-    layers.eachLayer(function(layer) {
-        if (list.indexOf(layer.feature.properties.line) !== -1) {
-            clusterGroup.addLayer(layer);
-        }
-    });
+  }).addTo(overlays);
+  // and add any markers that fit the filtered criteria to that group.
+  layers.eachLayer(function(layer) {
+    if (list.indexOf(layer.feature.properties.line) !== -1) {
+      clusterGroup.addLayer(layer);
+    }
+  });
 }
